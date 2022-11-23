91496a339d8e1e381f3817a59408628190820f97
subhas8055 <118911069+subhas8055@users.noreply.github.com>
2022-11-23 20:58:09 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,13 @@
+class Age{
+static int a;
+static int b=28;
+public static void main (String S[]){
+	for (int a=0;a<100;a++){
+ b=28+a;
+if (b==2*a){
+System.out.println("Son age is"+a+"\n"+"father age is "+b);}
+if(b==3*a){
+	System.out.println("Son age is"+a+"\n"+"father age is "+b);}
+	}
+}
+}
