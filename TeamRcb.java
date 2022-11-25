d0f29d3789d6c71129141773c057a6822e6f10ab
subhas8055 <subhas.xworkz@gmail.com>
2022-11-25 19:55:58 +0530

    message

---
+++ TeamRcb.java
@@ -0,0 +1,8 @@
+class TeamRcb {
+public static void main(String S[]){
+String Players[] = {"VIRAT","FAF","MAXI","RAJAT",};
+System.out.println(Players.length);
+System.out.println(Players[0]+"\n"+Players[1]+"\n"+Players[2]+"\n"+Players[3]);
+
+}
+}
