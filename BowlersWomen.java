f6043d3b553cb9dad2705adece207c12f8e7ac01
subhas8055 <118911069+subhas8055@users.noreply.github.com>
2022-11-25 19:14:14 +0530

    Add files via upload

---
+++ BowlersWomen.java
@@ -0,0 +1,9 @@
+class BowlersWomen{
+static String topBowlers[] ={"Sophie Ecclestone","Deepti Sharma","	Renuka Singh","	Sarah Glenn","	Shabnim Ismail","Hayley Matthews","Megan Schutt","Katherine Brunt","Jess Jonassen","Sneh Rana","Nonkululeko Mlaba","Amelia Kerr","Afy Fletcher","Salma Khatun","Inoka Ranaweera"};
+public static void main (String S[]){
+int points[] ={756,742,737,737,707,705,704,694,691,681,669,635,633,630,609};
+System.out.println("THE TOP RANKING BOWLERS WITH THEIR POINTS");
+System.out.println(topBowlers.length);
+System.out.println(topBowlers[0]+"="+points[0]+"\n"+topBowlers[1]+"="+points[1]+"\n"+topBowlers[2]+"="+points[2]+"\n"+topBowlers[3]+"="+points[3]+"\n"+topBowlers[4]+"="+points[4]+"\n"+topBowlers[5]+"="+points[5]+"\n"+topBowlers[6]+"="+points[6]+"\n"+topBowlers[7]+"="+points[7]+"\n"+topBowlers[8]+"="+points[8]+"\n"+topBowlers[9]+"="+points[9]+"\n"+topBowlers[10]+"="+points[10]+"\n"+topBowlers[11]+"="+points[11]+"\n"+topBowlers[12]+"="+points[12]+"\n"+topBowlers[13]+"="+points[13]+"\n"+topBowlers[14]+"="+points[14]);
+}
+}
