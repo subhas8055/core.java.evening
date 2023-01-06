60a49fd8472c208fe1ef471c47b8755ed211cc4f
subhas8055 <subhas.xworkz@gmail.com>
2023-01-06 20:13:54 +0530

    exam

--- answer/Multiples.java
+++ answer/Multiples.java
@@ -1,21 +1,14 @@
 class Multiples{
 public static void main(String [] S){
-System.out.println("the multiples of 5 are " );
+System.out.println("the multiples of 5 and 10 are " );
 for(int i=0;i<=100;i++){
-	if(i%5 ==0){
+	if(i%5 ==0 && i%10==0){
 	
 	System.out.println(i );
 	
 	}
 }
-System.out.println("the multiples of 10 are " );
-for(int i=0;i<=100;i++){
-	if(i%10 ==0){
-	
-	System.out.println(i );
-	
-	}
-	}
+
 
 }
 
