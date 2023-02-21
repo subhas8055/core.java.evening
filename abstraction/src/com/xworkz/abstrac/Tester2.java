9345dffd9ebfee4c789724ffd9a663b8f6888485
subhas8055 <subhas.xworkz@gmail.com>
2023-02-22 01:21:08 +0530

    with exception and arrays

---
+++ abstraction/src/com/xworkz/abstrac/Tester2.java
@@ -0,0 +1,12 @@
+package com.xworkz.abstrac;
+
+import com.xworkz.abstrac.chair.Dsh;
+import com.xworkz.abstrac.plastic.Plastic;
+
+public class Tester2 {
+public static void main(String[] args) {
+  Plastic  d = new Dsh();
+	d.use();
+//	((Dsh) d).doc();
+}
+}
---
+++ abstraction/src/com/xworkz/abstrac/chair/Table.java
@@ -0,0 +1,18 @@
+package com.xworkz.abstrac.chair;
+
+import com.xworkz.abstrac.plastic.Plastic;
+
+public   class Table implements Plastic{
+public String touse() {
+	System.out.println("use as a table");
+return null;}
+
+@Override
+public String use() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+
+}
