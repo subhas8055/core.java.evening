c41ac9f7bb40460754408e822d61060b0618f407
subhas8055 <subhas.xworkz@gmail.com>
2023-05-05 01:33:15 +0530

    afterget

--- mvc-structure/src/com/xworkz/mvc/repository/AmusementParkRepo.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.mvc.repository;
-
-import com.xworkz.mvc.dto.AmusementParkDTO;
-
-public interface AmusementParkRepo {
-
-	 public AmusementParkDTO  save(AmusementParkDTO dto);
-
-}
--- mvc-structure/src/com/xworkz/mvc/service/AmusementParkService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.mvc.service;
-
-import com.xworkz.mvc.dto.AmusementParkDTO;
-
-public interface AmusementParkService {
-
-	public AmusementParkDTO validateAndSave(AmusementParkDTO dto);
-
-}
